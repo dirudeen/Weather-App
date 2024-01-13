dfc597657c0e92eb7e3d0d2a2e3bebfaa94ab569
Hadiru Colley <dirudeen22@gmail.com>
2024-01-14 01:40:41 +0300

    Refactor API request to use axios instance
    Create axios instance for API requests, improving reusability and
    maintainability. Storing the base URL prevents repetition and potential
    inconsistency. This change ensures a centralized location for all API
    calls to AccuWeather. It also replaces the direct axios import within
    the API file with the axios instance call. No issue references.

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,12 +1,17 @@
-import axios from "axios"
+import axios from "axios";
+
+ const axiosIstance = axios.create({
+  baseURL: "http://dataservice.accuweather.com/",
+  timeout: 1000,
+});
 
 const apikey: string = import.meta.env.VITE_ACC_WEATHER_API_KEY
 
 if(!apikey) throw Error("api key is missing")
 
 export async function fetchCurrrentWeatherCondition(locationCode = 318251) {
-  const response = await axios.get(
-    `http://dataservice.accuweather.com/currentconditions/v1/${locationCode}?apikey=${apikey}`
+  const response = await axiosIstance.get(
+    `currentconditions/v1/${locationCode}?apikey=${apikey}`
   );
   return response;
 }
