e141121bd2388ca5e887ab3c611a1b055ea42ae4
Hadiru Colley <dirudeen22@gmail.com>
2024-01-14 01:27:38 +0300

    Add fetchCurrrentWeatherCondition function which use axios for fetching the up-to-date weather for a given locationCode

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -4,3 +4,9 @@ const apikey: string = import.meta.env.VITE_ACC_WEATHER_API_KEY
 
 if(!apikey) throw Error("api key is missing")
 
+export async function fetchCurrrentWeatherCondition(locationCode = 318251) {
+  const response = await axios.get(
+    `http://dataservice.accuweather.com/currentconditions/v1/${locationCode}?apikey=${apikey}`
+  );
+  return response;
+}
