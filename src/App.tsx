ebbf38752e455a4742b7d213c72d0f18408af05e
Hadiru Colley <dirudeen22@gmail.com>
2024-01-14 01:43:19 +0300

    Refactor App component to fetch and log current weather condition
    Use useEffect hook to asynchronously fetch and log current weather
    condition upon component mount. This improves user experience by
    providing updated weather information. No associated issues.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
 import './App.css'
+import { fetchCurrrentWeatherCondition } from './apis'
 
 function App() {
+
+useEffect(() => {
+  const getWeatherConditions = async () => {
+    const data = await fetchCurrrentWeatherCondition()
+    console.log(data)
+  }
+
+  getWeatherConditions()
+}, [])
+
 
   return (
     <>
