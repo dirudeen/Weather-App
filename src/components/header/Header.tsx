5c8edb33e9a62270aa1e48900612732c28097207
Hadiru Colley <dirudeen22@gmail.com>
2024-01-14 23:12:01 +0300

    Create Header component add SearchField component to Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import SearchField from "../search/SearchField";
+
+export default function Header() {
+  return (
+    <header className="p-4 flex place-items-center">
+        <SearchField />
+    </header>
+  )
+}
