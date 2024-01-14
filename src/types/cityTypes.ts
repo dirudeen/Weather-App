70ee42e06c9bf696bcdb891efe74363c31d94a3c
Hadiru Colley <dirudeen22@gmail.com>
2024-01-15 00:09:48 +0300

    Create CityInfoProps interface for representing city information. This new interface defines properties such as id, name, latitude, longitude, country code, timezone, country, and admin1. These properties will enable standardized handling of city data throughout the application. #Issue123

---
+++ src/types/cityTypes.ts
@@ -0,0 +1,10 @@
+export interface CityInfoProps {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  country_code: string;
+  timezone: string;
+  country: string;
+  admin1: string;
+}
