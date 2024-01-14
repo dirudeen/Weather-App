07496261f31883edfe11d9af6f5e151ff9f99ea1
Hadiru Colley <dirudeen22@gmail.com>
2024-01-14 23:49:25 +0300

    Refactor layout structure for improved modularity and organization
    Introduce Layout component to enhance modularity and readability by
    segregating header and main section. This change streamlines the
    component structure, aiding maintenance and future expansion.
    
    No related issues.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import Header from "./components/header/Header";
+import Layout from "./components/layout/Layout";
+
 function App() {
   return (
-    <main className="min-w-[312px] w-screen min-h-screen bg-slate-700">
-      <section>
-        {/* {HEADER} */}
+    <Layout>
+      <section className="outline-dashed outline-red-400 w-[60rem] bg-slate-900 mx-auto">
+        <Header />
 
         {/* {MAIN SECTION THAT CONTAINERE ALL THE GRID ITEMS} */}
       </section>
-    </main>
+    </Layout>
+    
   );
 }
 
