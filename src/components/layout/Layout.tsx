c4464c2325a8a90135fdccbe9b6308cf1217cabd
Hadiru Colley <dirudeen22@gmail.com>
2024-01-14 23:01:32 +0300

    Refactor Layout component to correctly render children

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,8 @@
 
-
-export default function Layout(Children: React.ReactNode) {
+export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <main className="min-w-[312px] w-screen min-h-screen bg-slate-900 flex place-items-center">
-      {Children}
+      {children}
     </main>
   );
 }
