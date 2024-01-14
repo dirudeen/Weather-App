cc0372e9cfd4f5389a4c73a0d6047a289c9b246b
Hadiru Colley <dirudeen22@gmail.com>
2024-01-14 23:48:55 +0300

    Refactor search field UI and functionality
    Refactor the search field UI to improve styling and add a placeholder
    for the city search feature. This change enhances user experience and
    makes the search functionality more intuitive.
    
    Issue #123

--- src/components/search/SearchField.tsx
+++ src/components/search/SearchField.tsx
@@ -1,14 +1,22 @@
-import { useState } from "react"
-import { CiSearch } from "react-icons/ci"
+import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
 
 export default function SearchField() {
-    const [value, setValue] = useState("")
+
+
+  const [value, setValue] = useState("");
   return (
-    <div className="flex items-center gap-4">
-        <CiSearch />
-        <form>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
-        </form>
+    <div className="flex items-center gap-1 rounded-md border-slate-400 border text-white px-4 py-2">
+      <CiSearch className="text-2xl text-white font-semibold" />
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="bg-transparent px-3 py-1 placeholder:text-slate-200 focus:outline-none"
+          placeholder="search for city"
+        />
+      </form>
     </div>
-  )
+  );
 }
