fbf18c8b695fb6136c6722fa679a10751202b45a
Hadiru Colley <dirudeen22@gmail.com>
2024-01-14 23:48:08 +0300

    Refactor background color value for improved visibility
    Updated the background color to enhance contrast for better user
    readability. This change ensures better accessibility and visual
    clarity. Fixes #123.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <main className="min-w-[312px] w-screen min-h-screen bg-slate-900 flex place-items-center">
+    <main className="min-w-[312px] w-screen min-h-screen bg-slate-950 flex place-items-center">
       {children}
     </main>
   );
