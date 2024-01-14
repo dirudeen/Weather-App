1ac673134ce27a836b9918560dcfb958d96b6ffc
Hadiru Colley <dirudeen22@gmail.com>
2024-01-15 01:02:04 +0300

    Create useDebounce hook for debouncing input values.
    This commit introduces a new useDebounce hook in useDebounce.tsx to
    debounce input values, enhancing the user experience by reducing
    unnecessary re-rendering and optimizing performance.

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export default function useDebounce(value: string) {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounceValue(value);
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value]);
+
+  return debounceValue;
+}
