de8c924819efa37835a68707555fb6aa76c5a242
Hadiru Colley <dirudeen22@gmail.com>
2024-01-15 16:49:01 +0300

    Refactor API to Add City Information Retrieval
    Add a new function to fetch city information and update types for
    response data. This change enhances the API by enabling retrieval of
    city information alongside weather data. It improves the functionality
    and data richness of the API. No associated issues.

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { CityInfoProps } from "../types/cityTypes";
 
  const forecastApi= axios.create({
   baseURL: "https://api.open-meteo.com/v1/",
@@ -17,4 +18,15 @@ export async function fetchCurrrentWeatherCondition({lat, long}: {lat: string, l
     `forecast?latitude=${lat}.73&longitude=${long}&current=temperature_2m,apparent_temperature,precipitation,wind_speed_10m,wind_direction_10m&timezone=auto`
   );
   return response.data;
+}
+interface ResponseType {
+  results: CityInfoProps[]
+}
+
+
+export async function fetchCityInfo(cityName: string = "kayseri") {
+  const response = await goeLocationApi.get<ResponseType>(
+    `search?name=${cityName}&count=10&language=en&format=json`
+  );
+  return response.data.results;
 }
