c6dd493c1106ac3e26ced74d842a62ea06a5e000
Hadiru Colley <dirudeen22@gmail.com>
2024-01-15 23:48:04 +0300

    Create city context and city context provider component.
    Fetch the city when the context mounts and update city state

--- src/provider/context.tsx
+++ src/provider/context.tsx
@@ -1,38 +1,30 @@
 import { ReactNode, createContext, useEffect, useState } from "react";
+import { fetchCityInfo } from "../apis";
 import { CityInfoProps } from "../types/cityTypes";
-import useFetchCityQuery from "../hooks/useFetchCityQuery";
 
 interface City {
-    city: CityInfoProps | null
-    onSetCity: (city: CityInfoProps) => void
+  city: CityInfoProps | null;
+  onSetCity: (city: CityInfoProps) => void;
 }
 
-type ContextProps = City | null
+type ContextProps = City | null;
 
-const CityContext= createContext<ContextProps>(null)
+export const CityContext = createContext<ContextProps>(null);
 
+export function CityContextProvider({ children }: { children: ReactNode }) {
+  //   const { data } = useFetchCityQuery("istanbul");
+  const [city, setCity] = useState<City["city"]>(null);
+  useEffect(() => {
+    const fetchCity = async () => {
+      const data = await fetchCityInfo();
+      return setCity(data[0]);
+    };
+    fetchCity();
+  }, []);
 
-export function ContextProvider ({children}: {children: ReactNode}) {
-    const [city, setCity] = useState<CityInfoProps | null>(null)
-    const {data} = useFetchCityQuery("istanbul")
-
-
-    useEffect(() => {
-      if(data){
-        setCity(data[0])
-      }
-    }, [])
-    
-
-
-
-    const value = {
-        city,
-        onSetCity: (city: CityInfoProps) => setCity(city)
-    }
-    return (
-        <CityContext.Provider value={value}>
-            {children}
-        </CityContext.Provider>
-    )
+  const value = {
+    city,
+    onSetCity: (cityData: CityInfoProps) => setCity(cityData),
+  };
+  return <CityContext.Provider value={value}>{children}</CityContext.Provider>;
 }
