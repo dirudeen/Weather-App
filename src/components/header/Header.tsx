1b78a13448d1ec6f6f78bfa6dfa21b74af8148d0
Hadiru Colley <dirudeen22@gmail.com>
2024-01-15 23:50:05 +0300

    Refactor header component style
    Improve header component style for better alignment and visual appeal by
    adding spacing to the border. This change enhances the overall UI.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import SearchField from "../search/SearchField";
 
 export default function Header() {
   return (
-    <header className="p-4 flex justify-center w-full border-green-400 border">
+    <header className="p-4 flex justify-center w-full border-green-400 border ">
         <SearchField />
     </header>
   )
