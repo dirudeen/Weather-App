f68e55de250411acaf93254fcc8b0d624b1ecd4f
Hadiru Colley <dirudeen22@gmail.com>
2024-01-15 23:44:57 +0300

    Refactor App.tsx to Fetch Current Weather Condition
    Use 'useEffect' to fetch weather condition based on the selected city
    from the context. This ensures that the weather data is dynamically
    retrieved when the city changes. Resolves #123

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useEffect } from "react";
 import Header from "./components/header/Header";
 import Layout from "./components/layout/Layout";
+import useCityContext from "./hooks/useCityContext";
+import { fetchCurrrentWeatherCondition } from "./apis";
+
+
 
 function App() {
+  const context = useCityContext()
+  const city = context?.city
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const lat = city!.latitude;
+      const long= city!.longitude
+       const result = await fetchCurrrentWeatherCondition({lat, long})
+       console.log(result)
+      
+    }
+    if(city){
+      fetchData()
+    }
+  }, [city])
+  
   return (
     <Layout>
       <section className="outline-dashed outline-red-400 w-[60rem] bg-slate-900 mx-auto">
@@ -10,7 +31,6 @@ function App() {
         {/* {MAIN SECTION THAT CONTAINERE ALL THE GRID ITEMS} */}
       </section>
     </Layout>
-    
   );
 }
 
