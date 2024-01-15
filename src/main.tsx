71f4fb5db3d0457249f7da901cf553d56077465b
Hadiru Colley <dirudeen22@gmail.com>
2024-01-15 23:43:43 +0300

    Add CityContextProvider for managing city information
    Introduce CityContextProvider to manage city-related information
    throughout the app for improved maintainability and accessibility. This
    allows seamless integration of city data across components. No
    associated issues.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { CityContextProvider } from "./provider/context.tsx";
 
 const client = new QueryClient({
   defaultOptions: {
@@ -19,7 +20,9 @@ const client = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
+      <CityContextProvider>
       <App />
+      </CityContextProvider>
       <ReactQueryDevtools buttonPosition="top-left"/>
     </QueryClientProvider>
   </React.StrictMode>
