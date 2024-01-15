76e50486fc8e3797247b18c1fa0fb0b82f0f955a
Hadiru Colley <dirudeen22@gmail.com>
2024-01-15 17:09:53 +0300

    Add ReactQueryDevtools for improved debugging capability
    Include ReactQueryDevtools in main.tsx to enhance debugging experience
    while working with React Query. This tool provides a visual interface
    for monitoring and debugging React Query hooks and cache instances. It
    offers greater visibility into the application's data fetching and
    caching processes. This change helps streamline development and
    troubleshooting efforts. No associated issue.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const client = new QueryClient({
   defaultOptions: {
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
       <App />
+      <ReactQueryDevtools buttonPosition="top-left"/>
     </QueryClientProvider>
   </React.StrictMode>
 );
